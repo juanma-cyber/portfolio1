e30bddb9b64833f27c052d8f1a483ea232feed11
juanma-cyber <psilosybe11@gmail.com>
2023-01-01 18:37:21 +0100

    machine commit

---
+++ src/content_option.js
@@ -0,0 +1,173 @@
+const logotext = "Juan";
+const meta = {
+    title: "Juan Ramírez",
+    description: "I’m Juan Ramírez data scientist _ Full stack devloper,currently working in Conservación Colombia",
+};
+
+const introdata = {
+    title: "I’m Juan Ramírez",
+    animated: {
+        first: "Graphic Design",
+        second: "Cool Websites",
+        third: "Develop Mobile apps and Web Sites",
+       
+    },
+    description: "I'm a systems and network technologist, web developer, with more than two years of experience creating websites, online stores, mobile applications, logo design, with JavaScript, HTML5, Tailwind, CSS, Bootstrap, React, php technologies. . , MySQL, Node.js, Wordpress. Graphic design with adobe illustrator, gimp, photoshop",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "Two years of experience creating web content, mobile applications, virtual stores, forms, databases, landing pages, e-commerce, logos, Graphic Design, (CMS-Wordpress) with technologies HTML5, JavaScript, css, php, python, React.js, React Native, Firebase, MySQL, Git-Hub, AWS, Cloud computing, A.I, Linux systems, Windows, computer security. ",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "Conservación Colombia",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Conservación Colombia",
+        date: "2021",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Conservación Colombia",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 75,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "UI UX designers create the user interface for an app, website, or other interactive media. Their work includes collaborating with product managers and engineers to gather requirements from users before designing ideas that can be communicated using storyboards. They also process flows or sitemaps.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "In a crowd of different apps available today on Google Play and App Store, it is important to highlight and attract as many people who will become loyal users and will not delete your app. One of the main things you should do is to have well-planned and quality content.Some experts say that content marketing is the king in digital marketing, so all you need is to use this information in the best possible way for your business. ",
+    },
+    {
+        title: "Wordpress Design",
+        description: "WordPress can be referred to as an online, open-source site-building tool based on PHP and MySQL programming languages. In more advanced terms it is called a content management system (CMS).",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "E-commerce",
+        link: "https://virtual-shop-ls8bi58n1-juanma-cyber.vercel.app",
+    },
+    {
+        img: "https://picsum.photos/500/800/?grayscale",
+        desctiption: "News site",
+        link: "https://vercel.com/juanma-cyber/noti",
+    },
+    {
+        img: "https://picsum.photos/600/700?grayscale",
+        desctiption: "Personal Portfolio1",
+        link: "https://vercel.com/juanma-cyber/portfolio",
+    },
+    {
+        img: "https://picsum.photos/700/600/?grayscale",
+        desctiption: "Cards, for posts or any presentation, with link button.",
+        link: "https://juanma-cyber.github.io/cards/",
+    },
+    {
+        img: "https://picsum.photos/800/600/?grayscale",
+        desctiption: "Blog for students",
+        link: "https://blog-kappa-inky.vercel.app/",
+    },
+    {
+        img: "https://picsum.photos/900/850/?grayscale",
+        desctiption: "Virtual Menu",
+        link: "https://menu-indol.vercel.app/",
+    },
+
+    {
+        img: "https://picsum.photos/250/700/?grayscale",
+        desctiption: "QuizzApp",
+        link: "https://quizz-app-sigma-eight.vercel.app/",
+    },
+    {
+        img: "https://picsum.photos/350/300/?grayscale",
+        desctiption: "Invoice-generator",
+        link: "https://invoice-generator-jade.vercel.app/",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "React app with Firebase",
+        link: "https://storable.vercel.app/add",
+    },
+    {
+        img: "https://picsum.photos/650/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/551/450?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/412/850/?grayscale",
+        desctiption: "App to save links whit React and Firebase",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "manuram0823@gmail.com",
+    YOUR_FONE: "+573105161238",
+    
+
+    
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://portfolio-puce-iota-63.vercel.app/",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+    youtube: "https://youtube.com",
+    whatsapp: "https://wa.me/573105161238",
+    
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
